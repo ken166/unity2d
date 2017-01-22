5f525a6fc5aeb2968378d9ce6081bf2eefc714c1
ken <rikj1234@gmail.com>
2017-01-22 18:38:16 +0900

    1/22

---
+++ 2d-shooting-game-master/Assets/Script/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	Spaceship spaceship;
+	// Use this for initialization
+	IEnumerator Start () {
+		spaceship = GetComponent<Spaceship> ();
+		while (true) {
+			spaceship.Shot (transform);
+			yield return new WaitForSeconds (spaceship.shotDelay);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxisRaw ("Horizontal");
+		float y = Input.GetAxisRaw ("Vertical");
+		Vector2 direction = new Vector2 (x, y).normalized;
+		spaceship.Move (direction);
+	}
+	void OnTriggerEnter2D(Collider2D c){
+		string layerName = LayerMask.LayerToName (c.gameObject.layer);
+		if (layerName == "Bullet(Enemy)") {
+			Destroy (c.gameObject);
+		}
+		if(layerName=="Bullet(Enemy)" || layerName == "Enemy"){
+			spaceship.Explosion ();
+			Destroy (gameObject);
+		}
+			
+	}
+}
---
+++ 2d-shooting-game-master/Assets/Script/Spaceship.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class Spaceship : MonoBehaviour {
+	public float speed;
+
+	//弾を鬱間隔
+	public float shotDelay;
+	public GameObject bullet;
+	public bool canShot;
+	public GameObject explosion;
+
+	public void Explosion(){
+		Instantiate (explosion, transform.position, transform.rotation);
+	}
+
+	public void Shot(Transform origin){
+		Instantiate (bullet, origin.position, origin.rotation);
+	}
+
+	public void Move(Vector2 direction){
+		GetComponent<Rigidbody2D> ().velocity = direction * speed;
+	}
+
+}

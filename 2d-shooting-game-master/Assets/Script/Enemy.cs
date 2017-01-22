2c8f1d69c585588659c88c391672d82af52f79c8
rikinoshita <ri_kinosita@yahoo.co.jp>
2017-01-22 18:59:23 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	2d-shooting-game-master/Library/CurrentLayout.dwlt
    
    #	2d-shooting-game-master/Library/ScriptAssemblies/Assembly-CSharp.dll.mdb
    #	2d-shooting-game-master/Library/assetDatabase3
    
    #	2d-shooting-game-master/Library/metadata/00/00000000000000003000000000000000
    
    #	2d-shooting-game-master/Library/metadata/00/00000000000000005100000000000000
    
    #	2d-shooting-game-master/Library/metadata/1f/1fbdec05a8de240c8a8db5129aaf5702
    
    #	2d-shooting-game-master/Library/metadata/39/39797370c93ad4f58ad6959567c3406c.info
    
    aa

---
+++ 2d-shooting-game-master/Assets/Script/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+	Spaceship spaceship;
+	// Use this for initialization
+	IEnumerator Start () {
+		spaceship = GetComponent<Spaceship> ();
+		spaceship.Move (transform.up * -1);
+
+		if (spaceship.canShot == false) {
+			yield break;
+		}
+		while (true) {
+			foreach(Transform child in transform){
+				Transform shotPosition = child.transform;
+				spaceship.Shot (shotPosition);
+			}
+			yield return new WaitForSeconds (spaceship.shotDelay);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter2D(Collider2D c){
+		string layerName = LayerMask.LayerToName (c.gameObject.layer);
+		if (layerName == "Bullet(Player)") {
+			Destroy (c.gameObject);
+			spaceship.Explosion ();
+			Destroy (gameObject);
+		}
+
+
+	}
+}
